211ad4856723ac1ddbf0a201368e8aa88da07859
Luismel Crosby <lmcr.com@gmail.com>
2019-09-30 23:05:16 -0400

    [LCROS-1] Cross Origin for localhost

--- src/main/java/org/lcrosby/ghostwordapi/controller/GhostWordController.java
+++ src/main/java/org/lcrosby/ghostwordapi/controller/GhostWordController.java
@@ -4,10 +4,7 @@ import org.lcrosby.ghostwordapi.model.WordResponse;
 import org.lcrosby.ghostwordapi.service.WordAnalyzer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -26,6 +23,7 @@ public class GhostWordController {
     @Autowired
     private WordAnalyzer wordAnalyzer;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(method = RequestMethod.GET, value = "/tell/{word}")
     @ResponseBody
     public WordResponse tell(@PathVariable("word")
